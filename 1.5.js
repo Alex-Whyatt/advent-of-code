b48f33939713e6c883fdd13711d29afc0c90d5c6
alex.whyatt@live.com <alex.whyatt@playerdata.co>
2022-01-21 16:09:01 +0000

    dos

--- 1.5.js
+++ 1.5.js
@@ -5,11 +5,11 @@ const inputNumbers = fs
     .split("\n")
     .filter((x) => Boolean(x))
     .map(Number);
-
+    
 let increaseCount = 0;
 
 for (let i = 0; i < inputNumbers.length; i++) {
-    if (inputNumbers[i+1] > inputNumbers[i]) {
+    if ((inputNumbers[i+1] + inputNumbers[i+2] + inputNumbers[i+3]) > (inputNumbers[i] + inputNumbers[i+1] + inputNumbers[i+2])) {
         increaseCount++;
     } 
 };
